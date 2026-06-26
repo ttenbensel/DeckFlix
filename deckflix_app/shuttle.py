9e41783d84290716b35790d80ef05285943ac20a
ttenbensel <tobias.tenbensel@gmail.com>
2026-06-26 20:57:24 +1000

    Use media inspection in shuttle module

--- deckflix_app/shuttle.py
+++ deckflix_app/shuttle.py
@@ -2,20 +2,7 @@ from pathlib import Path
 import shutil
 
 from deckflix_app.scanner import scan_videos
-from deckflix_app.health import clean_title
-
-
-def is_tv_file(path):
-    name = str(path).lower()
-
-    return (
-        "s01" in name
-        or "s02" in name
-        or "s03" in name
-        or "s04" in name
-        or "s05" in name
-        or "season" in name
-    )
+from deckflix_app.media import inspect_media
 
 
 def get_storage_info(path):
@@ -51,17 +38,23 @@ def scan_shuttle(shuttle_path):
             "files": [],
             "movies": [],
             "tv": [],
+            "media": [],
         }
 
     files = scan_videos(shuttle)
-    movies = []
-    tv = []
+    media_items = [inspect_media(file) for file in files]
 
-    for file in files:
-        if is_tv_file(file):
-            tv.append(file)
-        else:
-            movies.append(file)
+    movies = [
+        item
+        for item in media_items
+        if item.media_type == "movie"
+    ]
+
+    tv = [
+        item
+        for item in media_items
+        if item.media_type == "tv"
+    ]
 
     return {
         "connected": True,
@@ -70,27 +63,22 @@ def scan_shuttle(shuttle_path):
         "files": files,
         "movies": movies,
         "tv": tv,
+        "media": media_items,
     }
 
 
-def compare_to_library(shuttle_files, library_files):
-    library_titles = set()
-
-    for file in library_files:
-        title = clean_title(Path(file).parent.name)
-        if title:
-            library_titles.add(title)
+def compare_to_library(shuttle_media, library_files):
+    library_items = [inspect_media(file) for file in library_files]
+    library_keys = set(item.key for item in library_items if item.key)
 
     new_media = []
     duplicates = []
 
-    for file in shuttle_files:
-        title = clean_title(Path(file).parent.name)
-
-        if title in library_titles:
-            duplicates.append(file)
+    for item in shuttle_media:
+        if item.key in library_keys:
+            duplicates.append(item)
         else:
-            new_media.append(file)
+            new_media.append(item)
 
     return {
         "new_media": new_media,
