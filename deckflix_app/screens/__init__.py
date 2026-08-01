761cd809cd777fea3b3553053ea2a3fc2fdd4986
Tobias ten Bensel <deckflix@localhost>
2026-08-01 15:08:29 +1000

    refactor(ui): remove unused dashboard screen

--- deckflix_app/screens/__init__.py
+++ deckflix_app/screens/__init__.py
@@ -6,14 +6,12 @@ from .import_monitor import TerminalImportMonitor
 from .operation_dashboard import show_operation_dashboard
 from .approval_plan import (
     show_approval_plan,
-    show_managed_approval_plan,
 )
 from .decision_queue import (
     show_decision_queue,
     show_managed_decision_queue,
 )
 from .parser_diagnostics import show_parser_diagnostics
-from .shuttle import show_receive_shuttle
 
 __all__ = [
     "show_operating_modes",
@@ -22,10 +20,8 @@ __all__ = [
     "show_operation_history",
     "TerminalImportMonitor",
     "show_managed_decision_queue",
-    "show_managed_approval_plan",
     "show_operation_dashboard",
     "show_approval_plan",
     "show_decision_queue",
     "show_parser_diagnostics",
-    "show_receive_shuttle",
 ]
