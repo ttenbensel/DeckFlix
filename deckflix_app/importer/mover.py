29af5503f976d3eff6e6d3fc0f2114730f1a9f57
Tobias ten Bensel <deckflix@localhost>
2026-08-01 18:47:58 +1000

    fix(import): support cross-filesystem finalisation

--- deckflix_app/importer/mover.py
+++ deckflix_app/importer/mover.py
@@ -1,9 +1,15 @@
 from pathlib import Path
+import shutil
 
 
 def atomic_move(temp_file: Path, destination: Path) -> Path:
     destination.parent.mkdir(parents=True, exist_ok=True)
 
-    temp_file.replace(destination)
+    shutil.copy2(
+        temp_file,
+        destination,
+    )
+
+    temp_file.unlink()
 
     return destination
